89fefdf2cf62e86b91b96f92bc64743ae80c501f
example <alisayar0107@gmail.com>
2021-07-15 06:16:48 +0300

    nav links are working now

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
+// Toggle the nav
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
+});
+
+// When a link is clicked on nav
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
 });
